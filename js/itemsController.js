212d9f3fd2a2d6c0caeeefe70a0e43dffcde646d
Hambulanzee <brendapaolavillaruribe@gmail.com>
2026-04-17 16:01:07 -0600

    commit si

--- js/itemsController.js
+++ js/itemsController.js
@@ -1,20 +1,69 @@
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
+//Crear 10 objetos en JavaScript y almacenarlos en local  
+                             
 const productos = [
-  { id: 1, nombre: "Tabla Picnic", precio: 1200.00, imagen: "img/tabla-picnic.jpg" },
-  { id: 2, nombre: "----", precio: 00.00, imagen: "--/--.--" },
-  { id: 3, nombre: "----", precio: 00.00, imagen: "--/--.--" },
-  { id: 4, nombre: "----", precio: 00.00, imagen: "--/--.--" },
-  { id: 5, nombre: "----", precio: 00.00, imagen: "--/--.--" },
-  { id: 6, nombre: "----", precio: 00.00, imagen: "--/--.--" },
-  { id: 7, nombre: "----", precio: 00.00, imagen: "--/--.--" },
-  { id: 8, nombre: "----", precio: 00.00, imagen: "--/--.--" },
-  { id: 9, nombre: "----", precio: 00.00, imagen: "--/--.--" },
-  { id: 10, nombre: "----", precio: 00.00, imagen: "--/--.--" }
+{
+nombre: "---",
+precio: 00,
+categoria: "---",
+stock: 10,
+descripcion: "----",
+imagen: "-----"
+},
+
+{
+nombre: "---",
+precio: 00,
+categoria: "---",
+stock: 10,
+descripcion: "----",
+imagen: "-----"
+},
+
+{
+nombre: "---",
+precio: 00,
+categoria: "---",
+stock: 0,
+descripcion: "----",
+imagen: "-----"
+},
+
+{
+nombre: "---",
+precio: 00,
+categoria: "---",
+stock: 0,
+descripcion: "----",
+imagen: "-----"
+},
+
+{
+nombre: "---",
+precio: 00,
+categoria: "---",
+stock: 00,
+descripcion: "----",
+imagen: "-----"
+},
+
+{
+nombre: "---",
+precio: 00,
+categoria: "---",
+stock: 00,
+descripcion: "----",
+imagen: "-----"
+},
+
+{
+nombre: "---",
+precio: 00,
+categoria: "---",
+stock: 00,
+descripcion: "----",
+imagen: "-----"
+},
+
 ];
 
-
-function renderProductos(lista) {
-  const contenedor = document.querySelector('.grid-products');
-  if(!contenedor) return;
-  contenedor.innerHTML = "";
+localStorage.setItem("productos", JSON.stringify(productos));
